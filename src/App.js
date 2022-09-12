04b7738adaa576718ae486930f0581be24770f57
Test <Test@SH-DESK-5280.shai.local>
2022-09-12 17:43:03 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -9,28 +9,46 @@ import "./index.css";
 const { Footer } = Layout;
 
 const App = () => {
-  const [person, setPerson] = useState([]);
-  const [Spend, setSpend] = useState([]);
-  
-  // const [flag, setFlag] = useState(false);
-  // const [arr, setArr] = useState();
+  let person = {};
+  let spend = {};
+  const [lists, setLists] = useState();
+  const [item, setItem] = useState([]);
+  const [arr, setArr] = useState([]);
 
+  const [flag, setFlag] = useState(false);
 
-  // var val = JSON.parse(localStorage.getItem("Spend"));
-
-
-  const onFinish = (values) => {
-    setPerson(values)
-    console.log('personDetails', values);
-    setSpend(values)
-    console.log('Spend', values);
-  };
+  const onFinish = async (values) => {
+    setFlag(false);
+    person = localStorage.getItem("person")
+    console.log("personDetails", values);
+    let personArr={};
+    localStorage.setItem("personDetails", JSON.stringify(person));
+    personArr.person = values;
+    await setItem(personArr);
+    console.log("personArr "+JSON.stringify(personArr));
+    console.log("finish:: "+item);
+  }
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+  const onFinish1 = (values) => {
+    console.log("fuckittt2222"+item)
+    item.spend = values ;
+    let localArr = arr;
+    console.log("itemitem "+item);
+    localArr.push(item);
+
+    console.log("fuckittt2222 "+localArr)
+    setItem(item);
+    setArr(localArr);
+    console.log("checkcheck "+JSON.stringify(arr));
+    setFlag(true);
+  };
+  const onFinishFailed1 = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   useEffect(() => {
-    localStorage.setItem('personDetails',JSON.stringify(person));
-    localStorage.setItem('Spend',JSON.stringify(Spend));
+    localStorage.setItem("Spend", JSON.stringify(spend));
   });
   return (
     <Layout className="site-page-header">
@@ -91,21 +109,35 @@ const App = () => {
             </Form.Item>
 
             <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+                <CheckCircleTwoTone twoToneColor="#52c41a" />
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+
+        <Card title="Add Amount" bordered={true}>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
             wrapperCol={{
-              offset: 8,
               span: 16,
             }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish1}
+            onFinishFailed={onFinishFailed1}
+            autoComplete="off"
           >
-            <Button type="primary" htmlType="submit">
-              Submit
-              <CheckCircleTwoTone twoToneColor="#52c41a" />
-            </Button>
-          </Form.Item>
-            </Form>
-      </Card>
-      
-        <Card title="Add Amount" bordered={true}>
-          <Form>
             <Form.Item
               label="Food :"
               name="Food"
@@ -127,7 +159,7 @@ const App = () => {
                   message: "Type the Correct Amount!",
                 },
               ]}
-              >
+            >
               <Input />
             </Form.Item>
             <Form.Item
@@ -135,24 +167,25 @@ const App = () => {
                 offset: 8,
                 span: 16,
               }}
-              >
+            >
               <Button type="primary" htmlType="submit">
                 Submit
               </Button>
             </Form.Item>
           </Form>
         </Card>
-          </div>
-        <Card title="Spend Analyser" bordered={true}>
-        {
-        val.map((item) => {
-    return (
-      <div>
-        <p>Amount :{item.Amount}</p>
       </div>
-    );
-  })};
-        </Card>
+      <Card title="Spend Analyser" bordered={true}>
+        {flag ? (
+          arr.map((item,i) => {
+          return (
+          <div>
+            <p>Name : {item.person.Name}</p>
+             <p>Amount :{item.spend.Amount}</p>
+          </div>
+        )   
+        })):null}  
+      </Card>
       <Footer style={{ textAlign: "right" }}>
         <h1>Spend Analyser</h1>
         <p>©2022 Created by Art3mer</p>
@@ -160,4 +193,5 @@ const App = () => {
     </Layout>
   );
 };
+
 export default App;
